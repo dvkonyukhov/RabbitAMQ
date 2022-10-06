22587c9af05ed3d1dd80a3e683a192dfd02be9d9
dvkonyukhov <w1356@yandex.ru>
2022-10-06 23:51:33 +0300

    exchange

--- src/main/java/dkonyukhov/App.java
+++ src/main/java/dkonyukhov/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package dkonyukhov;
 
 /**
  * Hello world!
--- src/main/java/dkonyukhov/ProducerQueque.java
+++ src/main/java/dkonyukhov/ProducerQueque.java
@@ -1,4 +1,4 @@
-package org.example;
+package dkonyukhov;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -6,7 +6,7 @@ import com.rabbitmq.client.ConnectionFactory;
 
 import java.nio.charset.StandardCharsets;
 
-public class Send {
+public class ProducerQueque {
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
@@ -14,9 +14,9 @@ public class Send {
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            //channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            for (int i = 0; i < 100; i++) {
-                String message = "Hello World!" + String.valueOf(i);
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            for (int i = 0; i < 100000; i++) {
+                String message = "Hello World!" + i;
                 channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
                 //System.out.println(" [x] Sent '" + message + "'");
             }
